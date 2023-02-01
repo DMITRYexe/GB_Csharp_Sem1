b10007f34b06a90d80f5b6fcdb2e907ec57d86c8
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-01 23:53:07 +0300

    Project 3. MaxOfTwo

--- Project2_CkeckOnSquare/Program.cs
+++ Project2_CkeckOnSquare/Program.cs
@@ -1,26 +1,26 @@
 ﻿/*С клавиатуры вводятся два вещественных числа. 
 Проверять является ли одно из них квадратом второго (блок-схема)
-*/ 
+*/
 Console.WriteLine("Привет! Данное ПО проверяет не является ли одно число квадратом другого.");
 Console.Write("Для старта введи первое число: ");
 string? firstNum = Console.ReadLine();
 double f;
 checked
 {
-f=Convert.ToDouble(firstNum);
+  f = Convert.ToDouble(firstNum);
 }
 Console.Write("Введите второе число: ");
 string? SecondNum = Console.ReadLine();
 double s;
 checked
 {
-s=Convert.ToDouble(SecondNum);
+  s = Convert.ToDouble(SecondNum);
 }
-if (f*f==s)
+if (f * f == s)
 {
   Console.WriteLine($"{s} является квадратом {f}.");
 }
-else if (s*s==f)
+else if (s * s == f)
 {
   Console.WriteLine($"{f} является квадратом {s}.");
 }
---
+++ Project3_MaxOfTwo/Program.cs
@@ -0,0 +1,18 @@
+﻿// С клавиатуры вводятся два числа a и b. Найти максимальное из них. 
+Console.WriteLine("Привет! Это ПО ищет наибольшее число из двух.");
+Console.Write("Введите первое число: ");
+int a, b, max;
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+  max = a;
+  Console.WriteLine($"Наибольшее число: {max}");
+}
+else if (b > a)
+{
+  max = b;
+  Console.WriteLine($"Наибольшее число: {max}");
+}
+else Console.WriteLine("Числа равны");
