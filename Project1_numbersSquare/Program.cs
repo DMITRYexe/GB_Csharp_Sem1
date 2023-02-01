2756ff5c6d959ebe06628cf3fdb04c2e0f8c6add
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-01 03:01:50 +0300

    Project 1. Number's square

---
+++ Project1_numbersSquare/Program.cs
@@ -0,0 +1,9 @@
+﻿// 1.	С клавиатуры вводится целое число. Вывести квадрат числа (блок-схема)
+
+Console.Write("Введите число для возведения его в квадрат: ");
+string? s;
+s=Console.ReadLine();
+int a;
+a=Convert.ToInt32(s);
+Console.WriteLine(a*a);
+
