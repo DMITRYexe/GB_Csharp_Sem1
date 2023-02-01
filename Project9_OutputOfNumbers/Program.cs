42afa62651903b43b3a47387c9f1eb592ac68da8
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-02 01:36:26 +0300

    Project 9. Output of even numbers

---
+++ Project9_OutputOfNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿// 9. Вывести на экран четные числа от 1 до N
+Console.WriteLine("Привет! Это ПО выводит четные числа от 1 до N");
+Console.WriteLine("Введи N, где N - целое число: ");
+int i = 2, N;
+N = Convert.ToInt32(Console.ReadLine());
+if (N >= 2)
+{
+  while (i <= N)
+  {
+    Console.Write(i + " ");
+    i = i + 2;
+  }
+}
+else if (N < 2)
+{
+  i = 0; // Т.к. по условию отчет идет от 1, мы не должны учитывать двойку, если N отрицательно. 
+while (i >= N)
+  {
+    Console.Write(i + " ");
+    i = i - 2;
+  }
+}
+else
+{
+  Console.Write("Ошибка при вводе N");
+}
