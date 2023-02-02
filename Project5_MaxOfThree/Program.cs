3886a5ec3ef47d8cd5b0b6914ce833dac0a64359
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-02 04:52:06 +0300

    Project 5. Maximum of three

---
+++ Project5_MaxOfThree/Program.cs
@@ -0,0 +1,26 @@
+﻿// 5. С клавиатуры вводятся три числа. Найти максимальное из трех чисел
+Console.WriteLine("Привет! Данное ПО находит максимальное число их трех.");
+Console.Write("Введите первое число: ");
+int a,b,c,max;
+a=Convert.ToInt32(Console.ReadLine());
+max = a;
+Console.Write("Введите второе число: ");
+b=Convert.ToInt32(Console.ReadLine());
+if (b>a)
+{
+  max = b;
+}
+Console.Write("Введите третее число: ");
+c=Convert.ToInt32(Console.ReadLine());
+if (c>max)
+{
+  max = c;
+}
+if (c==a && b==a)
+{
+  Console.Write("Все числа одинаковы.");
+}
+else
+{
+  Console.Write($"Максимальное число: {max}");
+}
