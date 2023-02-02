06b508183de39d5a495eca510f2eef8e4eb65c78
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-02 05:51:53 +0300

    Project 7. Range output of numbers from -N to N

---
+++ Project8_RangeOutput/Program.cs
@@ -0,0 +1,26 @@
+﻿// 8. Вывести на экран числа от -N до N
+Console.WriteLine("Привет! Данная программа выводит все целые числа из диапазона [-N;N]");
+Console.Write("Задайте N: ");
+int N,Nminus,i;
+N = Convert.ToInt32(Console.ReadLine());
+if (N>=0)
+{
+Nminus=N*(-1);
+i=Nminus;
+while (i!=N+1)
+{
+  Console.Write($"{i} ");
+  i++;
+}
+}
+else
+{
+  Nminus=N;
+  N=N*(-1);
+  i=N;
+  while (i!=Nminus-1)
+{
+  Console.Write($"{i} ");
+  i--;
+}
+}
