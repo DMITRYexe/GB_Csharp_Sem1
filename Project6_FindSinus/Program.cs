d8311bdb35950c9c6677f51c5657417b6a149e4a
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-02 05:38:00 +0300

    Project 6. Finding the sine

---
+++ Project6_FindSinus/Program.cs
@@ -0,0 +1,13 @@
+﻿// 6. Написать программу вычисления значения функции y = sin(a). (Класс Math).  
+
+Console.WriteLine("Привет! Эта программа ищет синус для определенного числа");
+Console.Write("Введите число: ");
+double a, y, x;
+a = Convert.ToDouble(Console.ReadLine());
+y = Math.Sin(a);
+Console.WriteLine($"Если даны радианы, то sin = {y}");
+x = a * Math.PI / 180;
+y = Math.Sin(x);
+Console.WriteLine($"Если даны градусы, то sin = {y}");
+
+
