8ccdbf5733691c5d08026968f72b4e68dc55d672
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-02 16:53:56 +0300

    Project 13. Deleting second digit from number

---
+++ Project13_NumberWithoutSecDig/Program.cs
@@ -0,0 +1,14 @@
+﻿// 13. Удалить вторую цифру целого числа введенного с клавиатуры.
+Console.WriteLine("Привет! Данная программа удаляет вторую цифру из введенного числа");
+Console.WriteLine("Укажите число: ");
+string? num = Console.ReadLine();
+if (num.Length>1)
+{
+  num=num.Remove(num.Length-2,1);
+  Console.WriteLine($"Результат: {num}");
+}
+else
+{
+  Console.WriteLine("Ошибка. Число однозначное");
+}
+
