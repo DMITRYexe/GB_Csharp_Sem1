68ca72022f1d48f36953106b02a1ae6d8a66ea1b
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-02 17:51:20 +0300

    Project 15. Output third digit

---
+++ Project15_OutputThirdDig/Program.cs
@@ -0,0 +1,23 @@
+﻿/* С клавиатуры вводится целое число. 
+Вывести третью цифру числа или сообщить, что её нет (Вывести: NO).
+*/
+Console.WriteLine("Привет! Эта программа выводит третью цифру из целого числа.");
+Console.WriteLine("Укажите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i,answ;
+if (num>99)
+{
+i=num/100;
+answ=i%10;
+Console.WriteLine($"Результат: {answ}");
+}
+else if (num<-99)
+{
+i=num/100;
+answ=(i%10); // не стал переводить ответ в положительное число, т.к. считаю логичнее оставить ответ отрицательным
+Console.WriteLine($"Результат: {answ}");
+}
+else
+{
+  Console.WriteLine("NO");
+}
